8180dbfdbd6234c841429fa5927555a9075a7e6d
cf9874 <cf9874@gmail.com>
2022-12-05 17:35:13 +0900

    basic of styled-components

--- init/src/App.js
+++ init/src/App.js
@@ -1,7 +1,43 @@
 import styled from "styled-components";
 
+const Wrap = styled.div`
+  display: flex;
+`;
+const RedBox = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+
+const BlueBox = styled.div`
+  background-color: powderblue;
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span``;
+
 function App() {
-  return <div className="App">sss</div>;
+  return (
+    <>
+      <Wrap>
+        <RedBox>
+          <Text>asdas</Text>
+        </RedBox>
+        <BlueBox>
+          <Text>@@@</Text>
+        </BlueBox>
+      </Wrap>
+      <Wrap>
+        <BlueBox>
+          <Text>@@@</Text>
+        </BlueBox>
+        <RedBox>
+          <Text>asdasdsa</Text>
+        </RedBox>
+      </Wrap>
+    </>
+  );
 }
 
 export default App;
