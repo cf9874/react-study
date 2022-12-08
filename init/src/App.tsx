eafedcffa8de44bac21e676046f25c86784510fa
cf9874 <cf9874@gmail.com>
2022-12-08 18:37:37 +0900

    #

--- init/src/App.tsx
+++ init/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styled, { createGlobalStyle } from "styled-components";
 import Router from "./Routers";
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
 html, body, div, span, applet, object, iframe,
@@ -69,6 +69,7 @@ function App() {
     <>
       <GlobalStyle />
       <Router />
+      <ReactQueryDevtools initialIsOpen={true} />
     </>
   );
 }
