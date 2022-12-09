0b655725925fe3d162c33df26e8cbed18aa6b6aa
cf9874 <cf9874@gmail.com>
2022-12-09 22:58:23 +0900

    gh-pages

--- init/src/Routers.tsx
+++ init/src/Routers.tsx
@@ -3,7 +3,8 @@ import { Coin, Coins } from "./routes";
 
 function Router() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      {/*  PUBLIC_URL은 package.json의 homepage URL값 */}
       <Routes>
         <Route path="/:coinId/*" element={<Coin />} />
         <Route path="/:coinId" element={<Coin />} />
