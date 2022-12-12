0e66fa0afd97cad28cd3f0cb889764854e37bb3b
cf9874 <cf9874@gmail.com>
2022-12-12 17:17:19 +0900

    properties & eorror handling

--- init/src/TodoList.tsx
+++ init/src/TodoList.tsx
@@ -1,23 +1,36 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 function TodoList() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit, formState } = useForm();
+
+  const onValid = (data: any) => {
+    const { firstName, lastName, userName, email, password } = data;
+    console.log(data);
+  };
+  console.log("error", formState.errors);
 
   return (
     <div>
       <h1>Hi</h1>
-      <form>
-        <input {...register("firstName")} placeholder="Write your firstName" />
-        <br />
-        <input {...register("lastName")} placeholder="Write your lastName" />
-        <br />
-        <input {...register("userName")} placeholder="Write your userName" />
-        <br />
-        <input {...register("email")} placeholder="Write your email" />
-        <br />
-        <input {...register("password")} placeholder="Write your password" />
-        <br />
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "15px",
+        }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("firstName", {
+            required: "firstName is required",
+            minLength: { value: 10, message: "could you give me more 10?" },
+          })}
+          placeholder="Write your firstName"
+        />
+        <input {...register("lastName", { required: "lastName is required" })} placeholder="Write your lastName" />
+        <input {...register("userName", { required: "userName is required" })} placeholder="Write your userName" />
+        <input {...register("email", { required: "email is required" })} placeholder="Write your email" />
+        <input {...register("password", { required: "password is required" })} placeholder="Write your password" />
         <button>Add</button>
       </form>
     </div>
