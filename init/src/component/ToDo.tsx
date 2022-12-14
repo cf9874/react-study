4c66cdfecbd16c6e7d67fe2430a765e6ae980dc0
cf9874 <cf9874@gmail.com>
2022-12-14 14:26:30 +0900

    state update

--- init/src/component/ToDo.tsx
+++ init/src/component/ToDo.tsx
@@ -5,22 +5,22 @@ import { IToDo, toDoState } from "../atoms";
 function ToDo({ id, text, desc }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
 
-  //   const onClick = (newDesc: IToDo["desc"]) => {
-  //     if (newDesc === "DONE") {
-  //         ....
-  //     }
-  //   };
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((prev) => {
+      const index = prev.findIndex((e) => e.id === id);
+      const newTodo = { id, text, desc: name as "TODO" | "DONE" | "DOING" };
 
-  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    console.log(e.currentTarget.name);
+      return [...prev.slice(0, index), newTodo, ...prev.slice(index + 1)];
+    });
   };
+
   return (
-    <li>
-      <span> {text}</span>
-      {/* <button>{desc}</button> */}
-      {/* {desc !== "TODO" && <button onClick={() => onClick("TODO")}>TODO</button>}
-      {desc !== "DOING" && <button onClick={() => onClick("DOING")}>DOING</button>}
-      {desc !== "DONE" && <button onClick={() => onClick("DONE")}>DONE</button>} */}
+    <li style={{ display: "flex", gap: 15 }}>
+      <span style={{ minWidth: 150, width: 150 }}> {text}</span>
+
       {desc !== "TODO" && (
         <button name="TODO" onClick={onClick}>
           TODO
